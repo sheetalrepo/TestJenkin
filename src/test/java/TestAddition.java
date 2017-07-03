e042a9dc0dcabdd5b07dcf35b0a143a52f9ed719
sheetal singh <sheetalsingh@xebia.com>
2017-07-03 12:34:29 +0530

    testing jenkin job

---
+++ src/test/java/TestAddition.java
@@ -0,0 +1,22 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import com.Addition;
+
+public class TestAddition {
+
+	Addition obj;
+
+	@Before
+	public void init() {
+		obj = new Addition();
+	}
+
+	@Test
+	public void testAddition() {
+		System.out.println("Test about to start....");
+		int result = obj.add(10, 20);
+		Assert.assertTrue("add not working", result == 30);
+	}
+}
